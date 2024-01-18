41d38570d39832958edc975e74445c580990fcd0
RCO14 <154086624+RCO14@users.noreply.github.com>
2024-01-18 21:45:26 +0000

    Add files via upload

---
+++ app_help_desk/logoff.php
@@ -0,0 +1,17 @@
+<?php
+
+	session_start();
+
+
+
+	//remover indices do array de sessão
+	//unset()
+
+	//destruir a variavel de sessão
+	//session_destroy()
+
+	session_destroy();
+	header('Location: index.php');
+
+
+?>
---
+++ incorporando_scripts/inicio.php
@@ -0,0 +1,8 @@
+<?php
+	include("menu.php"); //se der erro dá warning
+
+	//require "menu2.php"; //se der erro dá Fatal Error
+?>
+
+Conteúdo da página (inicio)
+
